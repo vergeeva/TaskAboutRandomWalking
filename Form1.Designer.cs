39892603dc6473e4e7778a6b54477d2d25cfc91b
nastya <nastya.vergeeva@mail.ru>
2022-04-04 20:46:54 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace TaskAboutRandomWalking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.CountOfChallenge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.probability = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(16, 16);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(544, 155);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(413, 178);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(148, 26);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Рассчитать";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // CountOfChallenge
+            // 
+            this.CountOfChallenge.Location = new System.Drawing.Point(188, 178);
+            this.CountOfChallenge.Name = "CountOfChallenge";
+            this.CountOfChallenge.Size = new System.Drawing.Size(207, 26);
+            this.CountOfChallenge.TabIndex = 2;
+            this.CountOfChallenge.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество испытаний";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вероятность равна ≈";
+            // 
+            // probability
+            // 
+            this.probability.Location = new System.Drawing.Point(249, 217);
+            this.probability.Name = "probability";
+            this.probability.Size = new System.Drawing.Size(179, 26);
+            this.probability.TabIndex = 5;
+            this.probability.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 254);
+            this.Controls.Add(this.probability);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CountOfChallenge);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Вероятность равна ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox CountOfChallenge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox probability;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TaskAboutRandomWalking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int Count = 0;
+            //Кнопка "Рассчитать
+            int.TryParse(CountOfChallenge.Text, out int N);
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                int x = 0, y = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    int v = rnd.Next(1, 5);
+                    switch (v)
+                    {
+                        case 1:
+                            x--;
+                            break;
+                        case 2:
+                            y++;
+                            break;
+                        case 3:
+                            x++;
+                            break;
+                        case 4:
+                            y--;
+                            break;
+                    }
+                }
+                if (Math.Abs(x) <= 2 && Math.Abs(y) <= 2) Count++;
+            }
+            double r = Convert.ToDouble(Count) / Convert.ToDouble(N);
+            probability.Text =  r.ToString();
+        }
+    }
+}
